78ee92a1f0dd81574f45f11be8b5a3927b1e635e
Ogier Bouvier <ogier.bouvier@epfl.ch>
2016-09-26 20:26:08 +0200

    Modified script to build project before running

--- rabbits/bouvier-rousset-in/run.sh
+++ rabbits/bouvier-rousset-in/run.sh
@@ -4,4 +4,7 @@ set -eux
 
 [[ -x "$(which java)" ]] || (echo "Java not found!" && exit 1)
 
+[[ -x "$(which gradle)" ]] || (echo "Gradle not found!" && exit 1)
+
+gradle buildJar
 java -jar build/libs/bouvier-rousset-in-standalone.jar
