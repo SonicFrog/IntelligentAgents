1dcc1a90962750a4966f91a55b2767ffd5674a4c
Ogier Bouvier <ogier.bouvier@epfl.ch>
2016-10-25 21:00:44 +0200

    Changes to report and run script

--- deliberative/run.sh
+++ deliberative/run.sh
@@ -8,4 +8,4 @@ find -name '*.java' | xargs javac -Xlint:unchecked -cp ../logist/logist.jar
 mkdir -p bin
 rsync -a --delete --exclude='*.java' src/ bin/
 find src -not -name '*.java' -type f -delete
-java -enableassertions -jar ../logist/logist.jar config/deliberative.xml deliberative-astar
+java -enableassertions -jar ../logist/logist.jar config/deliberative.xml deliberative-naive
