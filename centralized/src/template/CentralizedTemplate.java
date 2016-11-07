dc29b2b8164abd7f666dd9e34eb6a7bbe02bbc0d
tharvik <tharvik@users.noreply.github.com>
2016-11-07 15:15:55 +0100

    generate initial solution

--- centralized/src/template/CentralizedTemplate.java
+++ centralized/src/template/CentralizedTemplate.java
@@ -2,10 +2,15 @@ package template;
 
 //the list of imports
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
 import logist.LogistSettings;
 
 import logist.Measures;
@@ -58,24 +63,91 @@ public class CentralizedTemplate implements CentralizedBehavior {
 		this.agent = agent;
 	}
 
-	private Map<Vehicle, List<Task>> find_best(TaskSet tasks) {
+	private Vehicle getBiggestVehicule(Set<Vehicle> vehicles) {
+		return vehicles.stream().max((a, b) -> a.capacity() - b.capacity()).get();
+	}
+
+	private int getSizeTask(Collection<Task> tasks) {
+		return tasks.stream().collect(Collectors.summingInt(t -> t.weight));
+	}
+
+	private void printMapVehicleListTask(Map<Vehicle, List<Task>> map) {
+		for (Map.Entry<Vehicle, List<Task>> entry : map.entrySet()) {
+			System.out.println(entry.getKey());
+			for (Task t : entry.getValue())
+				System.out.println("\t" + t);
+		}
+	}
+
+	private Map<Vehicle, List<Task>> fillVehicules(Set<Vehicle> vehicles, Set<Task> tasks) {
 		Map<Vehicle, List<Task>> current = new HashMap<>();
 
+		while (!vehicles.isEmpty() && !tasks.isEmpty()) {
+			Vehicle current_vehicle = getBiggestVehicule(vehicles);
+			List<Task> current_tasks = new ArrayList<>();
+
+			while (current_vehicle.capacity() > getSizeTask(current_tasks)) {
+				Task t = tasks.iterator().next();
+				tasks.remove(t);
+
+				current_tasks.add(t);
+			}
+
+			current.put(current_vehicle, current_tasks);
+
+			vehicles.remove(current_vehicle);
+		}
+
 		return current;
+	}
+
+	private Map<Vehicle, List<Task>> findBest(Set<Vehicle> vehicles, Set<Task> tasks) {
+		Map<Vehicle, List<Task>> current = fillVehicules(vehicles, tasks);
+
+		return current;
+	}
+
+	private City movePickupAndMoveDeliver(City current, Plan plan, Task task) {
+		for (City city : current.pathTo(task.pickupCity))
+			plan.appendMove(city);
+
+		plan.appendPickup(task);
+
+		for (City city : task.path())
+			plan.appendMove(city);
+
+		plan.appendDelivery(task);
+
+		return task.deliveryCity;
+	}
+
+	private Map<Vehicle, Plan> mapListTaskAsMapPlan(Map<Vehicle, List<Task>> map) {
+		Map<Vehicle, Plan> res = new HashMap<>();
+
+		for (Map.Entry<Vehicle, List<Task>> entry : map.entrySet()) {
+			Vehicle v = entry.getKey();
+			City current = v.getCurrentCity();
+			Plan p = new Plan(current);
+
+			for(Task t : entry.getValue())
+				current = movePickupAndMoveDeliver(current, p, t);
+
+			res.put(v, p);
+		}
+
+		return res;
 	}
 
 	@Override
 	public List<Plan> plan(List<Vehicle> vehicles, TaskSet tasks) {
 		long time_start = System.currentTimeMillis();
 
-		//		System.out.println("Agent " + agent.id() + " has tasks " + tasks);
-		Plan planVehicle1 = naivePlan(vehicles.get(0), tasks);
+		Map<Vehicle, List<Task>> best = findBest(new HashSet<>(vehicles), new HashSet<Task>(tasks));
+		Map<Vehicle, Plan> plans_best = mapListTaskAsMapPlan(best);
 
 		List<Plan> plans = new ArrayList<Plan>();
-		plans.add(planVehicle1);
-		while (plans.size() < vehicles.size()) {
-			plans.add(Plan.EMPTY);
-		}
+		for (Vehicle v : vehicles)
+			plans.add(plans_best.getOrDefault(v, Plan.EMPTY));
 
 		long time_end = System.currentTimeMillis();
 		long duration = time_end - time_start;
